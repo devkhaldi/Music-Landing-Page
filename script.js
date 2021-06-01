6f83d98e58ffc9f02a73627b1ba2a3969a5620c1
El-khaldi Abderrahim <54896891+devkhaldi@users.noreply.github.com>
2021-06-01 20:52:58 +0100

    toggle navbar

--- script.js
+++ script.js
@@ -6,11 +6,17 @@ window.onscroll = () => {
   // change navbar style on scroll
   if (window.scrollY !== 0) navbar.classList.add("nav-scrolling")
   else navbar.classList.remove("nav-scrolling")
-  // Toggle join now btn
 
+  // Toggle join now btn
   if (window.scrollY > joinNowBtn.offsetTop + 250)
     navbarBtn.classList.remove("hidden-btn")
   else navbarBtn.classList.add("hidden-btn")
+
+  // Hide Navbar
+  const section6 = document.querySelector(".section-6")
+  if (window.scrollY > section6.offsetTop - 550)
+    navbar.classList.add("hidden-nav")
+  else navbar.classList.remove("hidden-nav")
 }
 
 // Toggle menu
